45e56f9b75978aca10cfb93a56c729b64a1b0ad3
Manilde van der Oord <m.van.der.oord@amsterdam.nl>
2023-06-30 15:29:41 +0200

    onnodige code weggehaald en unit-test herschreven

--- src/bereikbaarheid/resources/lastbeperking_resource.py
+++ src/bereikbaarheid/resources/lastbeperking_resource.py
@@ -23,7 +23,7 @@ class LastbeperkingResource(ModelResource):
         row["lastbeperking_in_kg"] = convert_str(row["lastbeperking_in_kg"], "float")
 
     def skip_row(self, instance, original, row, import_validation_errors=None):
-        if row["lastbeperking_in_kg"] == None  or not row["lastbeperking_in_kg"]:
+        if not row["lastbeperking_in_kg"]:
             return True
 
         return super().skip_row(instance, original, row, import_validation_errors)
--- tests/test_bereikbaarheid/test_resource_utils.py
+++ tests/test_bereikbaarheid/test_resource_utils.py
@@ -9,23 +9,38 @@ class TestUtils:
 
     @pytest.mark.parametrize("test_input, expected", 
                              [("16/03/22 0:00", datetime.datetime(2022, 3, 16, 0, 0)), 
-                              ("2022-02-11 00:00:00.000", datetime.datetime(2022, 2, 11, 0, 0)),
-                              ("22-03-17", ValueError),
-                              ("test", ValueError)])
-    def test_convert_to_date(self, test_input, expected):
-        """Raise an exception if value can't be converted to datetime format"""
+                              ("2022-02-11 00:00:00.000", datetime.datetime(2022, 2, 11, 0, 0))])
+    def test_convert_to_date_pass(self, test_input, expected):
+        """Value with format %d/%m/%y %H:%M of %Y-%m-%d %H:%M:%S.%f, can be converted to datetime format"""
+        assert convert_to_date(test_input) == expected            
 
-        assert convert_to_date(test_input) == expected
+
+    @pytest.mark.parametrize("test_input, expected", 
+                              [("22-03-17", "verkeerd"), ("test", "verkeerd")])
+    def test_convert_to_date_exception(self, test_input, expected):
+        """Raise an exception if value can't be converted to datetime format"""
+        with pytest.raises(ValueError) as e:
+            convert_to_date(test_input)
+        assert str(e.value)[0:8] == expected
+
 
     @pytest.mark.parametrize("test_input, expected", 
                              [("12:00", datetime.time(12, 0)), 
                               ("13:00:59", datetime.time(13, 0, 59)),
-                              ("1", datetime.time(1, 0)),
-                              ("test", ValueError)])
-    def test_convert_to_time(self, test_input, expected):
+                              ("1", datetime.time(1, 0))])
+    def test_convert_to_time_pass(self, test_input, expected):
         """Raise an exception if string can't be converted to needed time format"""
-
         assert convert_to_time(test_input) == expected
+
+
+    @pytest.mark.parametrize("test_input, expected", 
+                             [("test", "verkeerd"),("10.05", "verkeerd")])
+    def test_convert_to_time_exception(self, test_input, expected):
+        """Raise an exception if string can't be converted to needed time format"""
+        with pytest.raises(ValueError) as e:
+            convert_to_time(test_input) 
+        assert str(e.value)[0:8] == expected
+
 
     @pytest.mark.parametrize("test_input, test_to, expected", 
                              [("1200", "float", 1200), 
