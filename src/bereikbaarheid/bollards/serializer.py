049831d9a6479e4692326656ba8f0b194ec20dc7
Manilde van der Oord <m.van.der.oord@amsterdam.nl>
2023-06-30 13:48:05 +0200

    endpoint bollards (vezips) toegevoegd

---
+++ src/bereikbaarheid/bollards/serializer.py
@@ -0,0 +1,55 @@
+from datetime import time
+
+from marshmallow import Schema, fields, validate, ValidationError, validates_schema
+
+from bereikbaarheid.validation import bbox_adam, days_of_the_week_abbreviated
+
+
+class BollardsSerializer(Schema):
+    day_of_the_week = fields.String(
+        required=False, 
+        data_key="dayOfTheWeek",
+        validate=validate.OneOf(days_of_the_week_abbreviated),
+    )
+
+    lat = fields.Float(
+        required=True,
+        validate=[
+            validate.Range(min=bbox_adam["lat"]["min"], max=bbox_adam["lat"]["max"])
+        ],
+    )
+
+    lon = fields.Float(
+        required=True,
+        validate=[
+            validate.Range(min=bbox_adam["lon"]["min"], max=bbox_adam["lon"]["max"])
+        ],
+    )
+
+    time_from = fields.Time(
+        format="%H:%M", load_default=time.min, required=False, data_key="timeFrom"
+    )
+
+    time_to = fields.Time(
+        format="%H:%M", load_default=time.max, required=False, data_key="timeTo"
+    )
+
+    @validates_schema
+    def validate_dates(self, data, **kwargs):
+        if any(x in data for x in ["time_to", "time_from"]):
+            return
+
+        if data["time_to"] < data["time_from"]:
+            raise ValidationError("timeTo must be later than timeFrom")
+
+    @validates_schema
+    def validate_field_dependencies(self, data, **kwargs):
+        dependent_fields = ["dayOfTheWeek", "timeFrom", "timeTo"]
+
+        if any(x in data for x in dependent_fields):
+            missing_fields = [f for f in dependent_fields if f not in data]
+
+            if missing_fields:
+                raise ValidationError(
+                    f"Missing fields: {', '.join(missing_fields)}"
+                )
