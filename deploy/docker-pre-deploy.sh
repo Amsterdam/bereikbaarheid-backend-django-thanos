1e184d1d2b9b18f4925521dfc4650e5b735782e6
kramer65 <petersanders65@gmx.com>
2023-09-26 10:19:14 +0200

    Revert "Added sleep in predeploy to inspect container"

--- deploy/docker-pre-deploy.sh
+++ deploy/docker-pre-deploy.sh
@@ -6,8 +6,6 @@ set -e   # stop on any error
 # check deploy settings to ensure we are deploying a correctly configured app
 python manage.py check --deploy #--fail-level WARNING
 
-sleep 600
-
 # migrate the db
 yes yes | python manage.py migrate --noinput
 
